03c48cb1b142d908f1f589ccd74a23f203f1e289
tmgasek <tomaszgasek28@gmail.com>
2022-02-04 21:52:55 +0000

    fixed typos

--- components/WikiBox.js
+++ components/WikiBox.js
@@ -2,14 +2,12 @@ export default function WikiBox({ wikiTitles, wikiLinks, num }) {
   return (
     <div className="md:w-1/3 h-auto ">
       {wikiTitles ? (
-        <h2 className="text-lg m-2 bg-accent1">
+        <h2 className="text-lg m-2 bg-accent1 pl-2">
           Wikipedia pages related to the number{' '}
           <span className="bg-accent2 m-1">{num}</span>
         </h2>
       ) : (
-        <h2 className="text-lg m-2 bg-accent1 animate-bounce">
-          start typing...
-        </h2>
+        <h2 className="text-lg m-2 bg-accent1 animate-bounce pl-2">...</h2>
       )}
 
       <div className="bg-accent2 break-words m-4 ">
